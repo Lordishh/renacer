99ce2c38c401fb1404f388d09db121385b434abc
Enuel <enuel.monzon@gmail.com>
2022-02-02 23:03:45 -0300

    Add theme switcher component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
 import { Link } from 'react-scroll';
 import styled from 'styled-components';
+import ThemeSwitcher from './ThemeSwitcher';
 
 const HeaderStyles = styled.header`
   position: fixed;
@@ -37,34 +39,133 @@ const HeaderStyles = styled.header`
       }
     }
   }
+  .navMenu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .menuIcon,
+  .closeIcon {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    margin-left: 10px;
+    padding: 3px;
+    svg {
+      color: var(--darkBlue_1);
+    }
+    &:hover {
+      background-color: #8080803b;
+      border-radius: 4px;
+    }
+  }
+  .closeIcon {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    &:hover {
+      background-color: #8080803b;
+      svg {
+        color: var(--white);
+      }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    nav {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      position: fixed;
+      width: 90%;
+      max-width: 250px;
+      top: 0;
+      right: 0%;
+      background-color: var(--lightBlue_2);
+      height: 100vh;
+      z-index: 100;
+      transform: translateX(100%);
+      transition: 1.5s ease-in-out transform;
+      overflow: hidden;
+    }
+    nav.open {
+      box-shadow: -1px 4px 10px 3px rgb(0 0 0 / 16%);
+      transform: translateX(0);
+    }
+    nav ul li {
+      display: block;
+      text-align: right;
+      width: 100%;
+      margin: 0.5rem 0;
+      a {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
 `;
 
-const Header = () => (
-  <HeaderStyles>
-    <div className="container">
-      <div className="navigation">
-        <h1 className="logo">Renacer</h1>
-        <div className="navMenu">
-          <nav>
-            <ul>
-              <li>
-                <Link to="inicio">Inicio</Link>
-              </li>
-              <li>
-                <Link to="entrenamientos">Entrenamientos</Link>
-              </li>
-              <li>
-                <Link to="nosotras">Nosotras</Link>
-              </li>
-              <li>
-                <Link to="contacto">Contacto</Link>
-              </li>
-            </ul>
-          </nav>
+const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 768px').matches
+  );
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setIsMobile(window.matchMedia('(max-width: 768px').matches);
+    });
+  }, []);
+
+  return (
+    <HeaderStyles>
+      <div className="container">
+        <div className="navigation">
+          <h1 className="logo">Renacer</h1>
+          <div className="navMenu">
+            <nav className={isMobile && isNavOpen ? 'open' : undefined}>
+              {isMobile && (
+                <div
+                  className="closeIcon"
+                  tabIndex="0"
+                  role="button"
+                  onClick={() => setIsNavOpen(false)}
+                  onKeyDown={() => setIsNavOpen(false)}
+                >
+                  <FiX />
+                </div>
+              )}
+              <ul>
+                <li>
+                  <Link to="inicio">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="entrenamientos">Entrenamientos</Link>
+                </li>
+                <li>
+                  <Link to="nosotras">Nosotras</Link>
+                </li>
+                <li>
+                  <Link to="contacto">Contacto</Link>
+                </li>
+              </ul>
+            </nav>
+            <ThemeSwitcher />
+            {isMobile && (
+              <div
+                className="menuIcon"
+                tabIndex="0"
+                role="button"
+                onClick={() => setIsNavOpen(true)}
+                onKeyDown={() => setIsNavOpen(true)}
+              >
+                <FiMenu />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </HeaderStyles>
-);
+    </HeaderStyles>
+  );
+};
 
 export default Header;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -8,20 +8,20 @@ import '@fontsource/poppins/700.css';
 import '@fontsource/poppins/800.css';
 
 const GlobalStyles = createGlobalStyle`
-  :root {
-    /* colors */
-    --darkBlue_1: #5FEBD8;
-    --darkBlue_2: #50B7A9;
-    --darkBlue_3: #112623;
-    --darkBlue_4: #0A1412;
-    --mediumSlateBlue: #127864;
-    --lightBlue_1: #C8F0E8;
-    --white: #FFFFFF;
-    --black: #000000;
-
-    /* Others */
+  :root{
+  /* colors */
+  --darkBlue_1: #3B447A;
+  --darkBlue_2: #222B5F;
+  --darkBlue_3: #0A0F19;
+  --darkBlue_4: #101826;
+  --mediumSlateBlue: #6C62E2;
+  --lightBlue_1: #F3F1FE;
+  --lightBlue_2: #ADBDE3;
+  --white: #FFFFFF;
+  --black: #000000;
+  /* others */
     --header-height: 50px;
-  }
+}
   html {
     font-size: 10px;
   }
@@ -29,7 +29,7 @@ const GlobalStyles = createGlobalStyle`
     background-color: var(---lightBlue_1);
     font-family: 'Poppins', sans-serif;
   }
-  *, *::after, *::before {
+  *, *:after, *:before {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -46,7 +46,7 @@ const GlobalStyles = createGlobalStyle`
     width: 90%;
     margin: 0 auto;
   }
-  img {
+  img, svg {
     width: 100%;
     height: 100%;
   }
