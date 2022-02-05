a4241f40cdbd428c0dddf579f67b0158e1d807d1
Enuel <enuel.monzon@gmail.com>
2022-02-05 03:40:52 -0300

    Add newsletter section component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutSection from '../components/AboutSection';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
+import NewsletterSection from '../components/NewsletterSection';
 import ServicesSection from '../components/services/ServicesSection';
 import TeamSection from '../components/team/TeamSection';
 
@@ -12,6 +13,7 @@ const LandingPage = () => (
     <ServicesSection />
     <AboutSection />
     <TeamSection />
+    <NewsletterSection />
   </div>
 );
 
