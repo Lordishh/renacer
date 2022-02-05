7fd603c7e1dafacb01eeac1b2b2acfe192083956
Enuel <enuel.monzon@gmail.com>
2022-02-04 22:11:45 -0300

    Add about section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-scroll';
+import PrimaryButton from './buttons/PrimaryButton';
+import ParagraphText from './paragraphTexts/ParagraphText';
+import SectionTitle from './titles/SectionTitle';
+import AboutImg from '../assets/images/about.png';
+
+const AboutStyles = styled.div`
+  padding: 10rem 0;
+  .about__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .about__img {
+    width: 600px;
+    margin-right: 5rem;
+  }
+  .about__subtitle {
+    margin-bottom: 1rem;
+    font-weight: 600;
+    text-transform: capitalize;
+    color: var(--mediumSlateBlue);
+  }
+  .about__title {
+    margin-bottom: 2rem;
+    max-width: 350px;
+  }
+  .about__desc {
+    margin-bottom: 2rem;
+    max-width: 300px;
+  }
+  @media only screen and (max-width: 768px) {
+    .about__wrapper {
+      flex-direction: column;
+    }
+    .about__img {
+      margin: 0 auto;
+      margin-bottom: 3rem;
+      max-width: 400px;
+    }
+  }
+`;
+
+function AboutSection() {
+  return (
+    <AboutStyles id="about">
+      <div className="container">
+        <div className="about__wrapper">
+          <div className="about__img">
+            <img src={AboutImg} alt="Artistic" />
+          </div>
+          <div className="about__info">
+            <ParagraphText className="about__subtitle">
+              ¿Quiénes somos?
+            </ParagraphText>
+            <SectionTitle className="about__title">
+              Trayectoría y objetivos
+            </SectionTitle>
+            <ParagraphText className="about__desc">
+              Renacer es un centro de pilates con cinco años de trayectoría.
+              Nuestro objetivo es ayudar a las personas a encontrar un
+              equilibrio, buscando aumentar la fuerza, la flexibilidad y
+              mejorando la concentración.
+            </ParagraphText>
+            <PrimaryButton buttonType={Link} to="contact" smooth>
+              Get In Touch
+            </PrimaryButton>
+          </div>
+        </div>
+      </div>
+    </AboutStyles>
+  );
+}
+
+export default AboutSection;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutSection from '../components/AboutSection';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import ServicesSection from '../components/services/ServicesSection';
@@ -8,6 +9,7 @@ const LandingPage = () => (
     <Header />
     <HeroSection />
     <ServicesSection />
+    <AboutSection />
   </div>
 );
 
