2602e25a131e1a185d1881536e4a3587942d1964
Enuel <enuel.monzon@gmail.com>
2022-02-06 08:51:06 -0300

    modified title component

--- src/components/titles/HeroTitle.js
+++ src/components/titles/HeroTitle.js
@@ -7,7 +7,6 @@ const TitleStyles = styled.h1`
   color: ${({ theme: { theme } }) =>
     theme === themeList.light ? 'var(--darkBlue_2)' : 'var(--lightBlue_1)'};
   font-weight: 900;
-  text-transform: capitalize;
   @media only screen and (max-width: 768px) {
     font-size: 4rem;
   }
